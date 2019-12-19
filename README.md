f91c00b41e2590cd46816ad4144fc6f93a626a76
sangkny <sangkny@yahoo.com>
2019-12-19 18:58:41 +0900

    README.md

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ In addition, plotAccLoss.py can plot the loss information from the training log 
 ./darknet detector train /path/to/xxx.data /path/to/xxx.cfg .path/to/xxx_pretrained_weights_or_intermediate_weights_file > train.log
 
 ##### Under ubuntu training,
-    train.txt val.txt should have only linefeed (\n) ended format unlike LF\CR in windows.
+    train.txt val.txt should have only linefeed (\r) ended format in linux unlike LF\CR in windows.
     Absolute path is safe. However, relative path can be used.
 ##### Under Windows training,
     Again, be careful and check the text file format if it has \lf\cr at the end of each line.
--- splitTrainAndTest.py
+++ splitTrainAndTest.py
@@ -5,9 +5,12 @@ import sys
 
 
 def split_data_set(image_dir):
+    use_target_dir = True
+    use_in_windows = False
+
     f_val = open("C:/Users/mmc/workspace/yolo/data/itms/itms_test.txt", 'w')
     f_train = open("C:/Users/mmc/workspace/yolo/data/itms/itms_train.txt", 'w')
-    use_target_dir = True
+
     target_dir = "/workspace/yolo/data/itms/images"
     path, dirs, files = next(os.walk(image_dir))
     data_size = len(files)
@@ -22,14 +25,26 @@ def split_data_set(image_dir):
 
             if ind in test_array:
                 if(use_target_dir):
-                    f_val.write(target_dir + '/' + f + '\n')  # windows for unix '\r'
+                    if(use_in_windows):
+                        f_val.write(target_dir + '/' + f + '\n')  # windows for unix '\r'
+                    else:
+                        f_val.write(target_dir + '/' + f + '\r')  # windows for unix '\r'
                 else:
-                    f_val.write(image_dir + '/' + f + '\n') # windows for unix '\r'
+                    if(use_in_windows):
+                        f_val.write(image_dir + '/' + f + '\n') # windows for unix '\r'
+                    else:
+                        f_val.write(image_dir + '/' + f + '\r')  # windows for unix '\r'
             else:
                 if(use_target_dir):
-                    f_train.write(target_dir + '/' + f + '\n')
+                    if(use_in_windows):
+                        f_train.write(target_dir + '/' + f + '\n')
+                    else:
+                        f_train.write(target_dir + '/' + f + '\r')
                 else:
-                    f_train.write(image_dir + '/' + f + '\n')
+                    if(use_in_windows):
+                        f_train.write(image_dir + '/' + f + '\n')
+                    else:
+                        f_train.write(image_dir + '/' + f + '\r')
 
 
 split_data_set(sys.argv[1])
