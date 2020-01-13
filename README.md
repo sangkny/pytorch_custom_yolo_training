aad18495489a3f48728857110d472ec58234089d
sangkny <sangkny@yahoo.com>
2020-01-13 19:28:57 +0900

    update-detail how to

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## Training YOLO with Custom Dataset in PyTorch
 ##### The contents came mostly from the previous owner. More functions for training Yolo have been added.
 - `splitTrainAndTest.py` splits the given files into training and testing data as createlist.py can do. 
-- In addition, `plotAccLoss.py` can plot the loss information from the training log file by darknet with 
-- `./darknet detector train /path/to/xxx.data /path/to/xxx.cfg .path/to/xxx_pretrained_weights_or_intermediate_weights_file > train.log`
+- `splitTrainAndTest.py` shuffles the data before splitting.
+- In addition, `plotAccLoss.py` can plot the loss information from the training log file by darknet with_ 
+- `./darknet detector train /path/to/xxx.data /path/to/xxx.cfg .path/to/xxx_pretrained_weights_or_intermediate_weights_file > train.log` for `silent mode`
+- `./darknet detector train /path/to/xxx.data /path/to/xxx.cfg .path/to/xxx_pretrained_weights_or_intermediate_weights_file 2>&1 | tee train.log` for `display mode`
 
 ##### Test
 - To test the model, please use `python object_detect_yolov3.py` after editting the file for a target image
@@ -11,10 +13,13 @@
 > > + IOU computation and ROI selection algorithm has been included 
 
 ##### Under ubuntu training,
-    train.txt val.txt should have only linefeed (\r) ended format in linux unlike LF\CR in windows.
+    train.txt val.txt should have only linefeed (\n) ended format in linux unlike \r\n in windows.
     Absolute path is safe. However, relative path can be used.
 ##### Under Windows training,
     Again, be careful and check the text file format if it has \lf\cr at the end of each line.
+    note that only '\r' for mac system
+##### Detail Reference -----------------------
+- **Darknet_Custom_Training_A2Z.txt** in the working folder
 
 Full story:
 https://towardsdatascience.com/training-yolo-for-object-detection-in-pytorch-with-your-custom-dataset-the-simple-way-1aa6f56cf7d9
--- splitTrainAndTest.py
+++ splitTrainAndTest.py
@@ -1,17 +1,29 @@
 import random
 import os
-import subprocess
-import sys
+#import subprocess
+import argparse
+#import sys
 
+parser = argparse.ArgumentParser(description='Spit data into train and test lists for YOLOv3')
+parser.add_argument('--image_dir', help='Path to image files.')
+parser.add_argument('--target_dir', type=str, default='/workspace/yolo/data/itms/images', help='relative path for ubuntu training')
+parser.add_argument('--use_target_dir', type=int, default=1, help='Flag to use target dir instead of using img_dir')
+parser.add_argument('--use_in_windows', type=int, default=0, help='Flag to use it in windows')
+args = parser.parse_args()
 
-def split_data_set(image_dir):
-    use_target_dir = True
-    use_in_windows = False
+# temp parameter settings
+args.image_dir = "C:/Users/mmc/workspace/yolo/data/itms/images"     # absolute path which contains origial images
+args.target_dir = "/workspace/yolo/data/itms/images"                # relative path in ubuntu
+args.use_target_dir = True
+args.use_in_windows = False
 
-    f_val = open("C:/Users/mmc/workspace/yolo/data/itms/itms_test.txt", 'w')
-    f_train = open("C:/Users/mmc/workspace/yolo/data/itms/itms_train.txt", 'w')
-
-    target_dir = "/workspace/yolo/data/itms/images"
+def split_data_set(arg):
+    use_target_dir = arg.use_target_dir
+    use_in_windows = arg.use_in_windows
+    f_val = open("C:/Users/mmc/workspace/yolo/data/itms/itms_val_20200117.txt", 'w')
+    f_train = open("C:/Users/mmc/workspace/yolo/data/itms/itms_train_20200117.txt", 'w')
+    image_dir = arg.image_dir
+    target_dir = arg.target_dir # ""
     path, dirs, files = next(os.walk(image_dir))
     data_size = len(files)
 
@@ -26,25 +38,27 @@ def split_data_set(image_dir):
             if ind in test_array:
                 if(use_target_dir):
                     if(use_in_windows):
-                        f_val.write(target_dir + '/' + f + '\n')  # windows for unix '\r'
+                        f_val.write(target_dir + '/' + f + '\r\n')  # windows for unix '\n'
                     else:
-                        f_val.write(target_dir + '/' + f + '\r')  # windows for unix '\r'
+                        f_val.write(target_dir + '/' + f + '\n')  # windows for unix '\n'
                 else:
                     if(use_in_windows):
-                        f_val.write(image_dir + '/' + f + '\n') # windows for unix '\r'
+                        f_val.write(image_dir + '/' + f + '\r\n') # windows for unix '\r'
                     else:
-                        f_val.write(image_dir + '/' + f + '\r')  # windows for unix '\r'
+                        f_val.write(image_dir + '/' + f + '\n')  # windows for unix '\n'
             else:
                 if(use_target_dir):
                     if(use_in_windows):
-                        f_train.write(target_dir + '/' + f + '\n')
+                        f_train.write(target_dir + '/' + f + '\r\n')
                     else:
-                        f_train.write(target_dir + '/' + f + '\r')
+                        f_train.write(target_dir + '/' + f + '\n')
                 else:
                     if(use_in_windows):
-                        f_train.write(image_dir + '/' + f + '\n')
+                        f_train.write(image_dir + '/' + f + '\r\n')
                     else:
-                        f_train.write(image_dir + '/' + f + '\r')
+                        f_train.write(image_dir + '/' + f + '\n')  # \r only for mac
+    f_val.close()
+    f_train.close()
 
 
-split_data_set(sys.argv[1])
+split_data_set(args)
